56c4d98b416ad2896139aae8d49116602ac45359
OmnixTry <Omnix.try@gmail.com>
2020-11-02 18:50:39 +0300

    Add Unit of Work With Entity Framework

---
+++ EnglishTrainer.DAL/Repositories/EFVocabularyUnitOfWork.cs
@@ -0,0 +1,78 @@
+﻿using EnglishTrainer.DAL.EF;
+using EnglishTrainer.DAL.Entities;
+using EnglishTrainer.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnglishTrainer.DAL.Repositories
+{
+    class EFVocabularyUnitOfWork : IVocabularyUnitOfWork
+    {
+        private VocabularyContext db;
+        private WordRepository wordRepository;
+        private TopicRepository topicRepository;
+        private UserRepository userRepository;
+        private MistakeRepository mistakeRepository;
+        public IRepository<Word> Words
+        {
+            get
+            {
+                if (wordRepository == null)
+                    wordRepository = new WordRepository(db);
+                return wordRepository;
+            }
+        }
+
+        public IRepository<Topic> Topics
+        {
+            get
+            {
+                if (topicRepository == null)
+                    topicRepository = new TopicRepository(db);
+                return topicRepository;
+            }
+        }
+
+        public IRepository<User> Users
+        {
+            get
+            {
+                if (userRepository == null)
+                    userRepository = new UserRepository(db);
+                return userRepository;
+            }
+        }
+
+        public IRepository<Mistake> Mistakes
+        {
+            get
+            {
+                if (mistakeRepository == null)
+                    mistakeRepository = new MistakeRepository(db);
+                return mistakeRepository;
+            }
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
+}
