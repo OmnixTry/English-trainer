ef3ac6f76ce83d41da722d63d24bd7cacf59a387
OmnixTry <Omnix.try@gmail.com>
2020-11-02 15:00:54 +0300

    Data Axess layer created with entities

---
+++ EnglishTrainer.DAL/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnglishTrainer.DAL.Entities
+{
+    class User
+    {
+        public int Id { get; set; }
+        // not null unique
+        public string NickName { get; set; }
+        public ICollection<Mistake> Mistakes { get; set; }
+    }
+}
