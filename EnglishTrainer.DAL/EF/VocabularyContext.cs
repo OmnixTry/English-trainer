75260c4e4b097fc71e02cd5ecb013b2f239927e2
OmnixTry <Omnix.try@gmail.com>
2020-11-02 15:08:12 +0300

    Data Access Layer Added with basic Entities

---
+++ EnglishTrainer.DAL/EF/VocabularyContext.cs
@@ -0,0 +1,32 @@
+﻿using EnglishTrainer.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnglishTrainer.DAL.EF
+{
+    class VocabularyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Word> Words { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Mistake> Mistakee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasAlternateKey(u => u.NickName);
+
+        }
+
+        public VocabularyContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=Vocabulary; Integrated Security= SSPI");
+        }
+    }
+}
---
+++ EnglishTrainer.DAL/Entities/Topic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnglishTrainer.DAL.Entities
+{
+    class Topic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Word> Words { get; set; }
+        public Topic()
+        {
+            Words = new List<Word>();
+        }
+    }
+}
